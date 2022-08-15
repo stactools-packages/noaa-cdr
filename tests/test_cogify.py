21e54ad0a001b8e6df91e526cf9db4d20c903d46
Pete Gadomski <pete.gadomski@gmail.com>
2022-08-15 06:55:11 -0600

    refactor: test structure for cogify

--- tests/test_cogify.py
+++ tests/test_cogify.py
@@ -1,12 +1,19 @@
 import os.path
 from tempfile import TemporaryDirectory
 
+import pytest
+
 from stactools import noaa_cdr
+from tests import test_data
+
+COGIFY_PARAMETERS = [("heat_content_anomaly_0-2000_yearly.nc", 17)]
 
 
-def test_cogify(netcdf_path_for_cogify: str) -> None:
+@pytest.mark.parametrize("infile,num_cogs", COGIFY_PARAMETERS)
+def test_cogify(infile: str, num_cogs: int) -> None:
+    external_data_path = test_data.get_external_data(infile)
     with TemporaryDirectory() as temporary_directory:
-        paths = noaa_cdr.cogify(netcdf_path_for_cogify, temporary_directory)
-        assert len(paths) == 17
+        paths = noaa_cdr.cogify(external_data_path, temporary_directory)
+        assert len(paths) == num_cogs
         for path in paths:
             assert os.path.exists(path)
