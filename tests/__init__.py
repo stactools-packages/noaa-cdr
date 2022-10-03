8000b0829cf63eb288396bf785cb1b22849dbcd3
Pete Gadomski <pete.gadomski@gmail.com>
2022-10-03 11:20:59 -0600

    refactor, tests: make cli testing simpler

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,12 @@
 import os.path
-from typing import Any, Dict
+from typing import Any, Dict, Sequence, Union
 
+import click
+from click.testing import CliRunner, Result
 from stactools.testing.test_data import TestData
 
 from stactools.noaa_cdr import ocean_heat_content
+from stactools.noaa_cdr.commands import create_noaa_cdr_command
 
 external_data: Dict[str, Any] = {
     "oisst-avhrr-v02r01.20220913.nc": {
@@ -27,3 +30,19 @@ for href in ocean_heat_content.iter_noaa_hrefs():
 
 
 test_data = TestData(__file__, external_data)
+
+
+@click.group()
+def test_cli() -> None:
+    pass
+
+
+create_noaa_cdr_command(test_cli)
+
+
+def run_command(command: Union[str, Sequence[str]]) -> Result:
+    runner = CliRunner()
+    result = runner.invoke(test_cli, command, catch_exceptions=False)
+    if result.output:
+        print(result.output)
+    return result
