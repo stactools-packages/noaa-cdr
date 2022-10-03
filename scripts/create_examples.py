0601251ba8c84982ef9268d6a1618a4c9b0317cd
Pete Gadomski <pete.gadomski@gmail.com>
2022-10-03 12:50:00 -0600

    chore: update examples

--- scripts/create_examples.py
+++ scripts/create_examples.py
@@ -71,9 +71,13 @@ with TemporaryDirectory() as temporary_directory:
 
     print("Saving catalog...")
     catalog.normalize_hrefs(examples)
-    stactools.core.copy.move_all_assets(
-        catalog, make_hrefs_relative=True, copy=True, ignore_conflicts=True
-    )
+    for item in catalog.get_all_items():
+        for asset in item.assets.values():
+            if asset.href.startswith(temporary_directory):
+                stactools.core.copy.move_asset_file_to_item(
+                    item, asset.href, copy=False, ignore_conflicts=True
+                )
+        item.make_asset_hrefs_relative()
     catalog.save(catalog_type=CatalogType.SELF_CONTAINED)
 
     print("Done!")
