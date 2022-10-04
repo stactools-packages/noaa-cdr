02b00a7def86c6b94a09624ebad0fa59c5558d49
Pete Gadomski <pete.gadomski@gmail.com>
2022-10-04 15:55:19 -0600

    feat: add sea ice concentration cogging

--- tests/sea_ice_concentration/test_commands.py
+++ tests/sea_ice_concentration/test_commands.py
@@ -15,6 +15,16 @@ def test_create_item(tmp_path: Path) -> None:
     item.validate()
 
 
+def test_create_item_with_cogs(tmp_path: Path) -> None:
+    path = test_data.get_path("data-files/seaice_conc_daily_nh_20211231_f17_v04r00.nc")
+    result = run_command(
+        f"noaa-cdr sea-ice-concentration create-item --cogs {path} {tmp_path}/out.json"
+    )
+    assert result.exit_code == 0, result.output
+    item = Item.from_file(str(tmp_path / "out.json"))
+    item.validate()
+
+
 def test_create_collection(tmp_path: Path) -> None:
     result = run_command(
         f"noaa-cdr sea-ice-concentration create-collection {tmp_path}/out.json"
