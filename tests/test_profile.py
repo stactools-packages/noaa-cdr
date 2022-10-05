db54fee435d32e34ddada75050fb558aea612b36
Pete Gadomski <pete.gadomski@gmail.com>
2022-10-05 14:05:44 -0600

    fix: squeeze data array before getting shape

---
+++ tests/test_profile.py
@@ -0,0 +1,12 @@
+import xarray
+
+from stactools.noaa_cdr.profile import BandProfile
+
+from . import test_data
+
+
+def test_shape() -> None:
+    path = test_data.get_path("data-files/seaice_conc_daily_nh_20211231_f17_v04r00.nc")
+    with xarray.open_dataset(path) as dataset:
+        band_profile = BandProfile.build(dataset, "cdr_seaice_conc")
+    assert band_profile.shape == [448, 304]
