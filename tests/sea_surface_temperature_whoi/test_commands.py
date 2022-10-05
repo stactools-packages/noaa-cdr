01fbcf702e9a8179293fcf0a60a4a5b7513f737d
Pete Gadomski <pete.gadomski@gmail.com>
2022-10-05 09:57:35 -0600

    feat: create items w/ cogs for whoi sst

--- tests/sea_surface_temperature_whoi/test_commands.py
+++ tests/sea_surface_temperature_whoi/test_commands.py
@@ -1,34 +1,21 @@
 from pathlib import Path
 
-import pytest
-from pystac import Collection, Item
+from pystac import Collection, ItemCollection
 
 from .. import run_command, test_data
 
 
-def test_create_item(tmp_path: Path) -> None:
+def test_create_items(tmp_path: Path) -> None:
     path = test_data.get_external_data(
         "SEAFLUX-OSB-CDR_V02R00_SST_D20210831_C20211223.nc"
     )
     result = run_command(
-        f"noaa-cdr sea-surface-temperature-whoi create-item {path} {tmp_path}/out.json"
+        f"noaa-cdr sea-surface-temperature-whoi create-items {path} {tmp_path}/out.json"
     )
     assert result.exit_code == 0, result.output
-    item = Item.from_file(str(tmp_path / "out.json"))
-    item.validate()
-
-
-@pytest.mark.xfail
-def test_create_item_with_cogs(tmp_path: Path) -> None:
-    path = test_data.get_external_data(
-        "SEAFLUX-OSB-CDR_V02R00_SST_D20210831_C20211223.nc"
-    )
-    result = run_command(
-        f"noaa-cdr sea-surface-temperature-whoi create-item --cogs {path} {tmp_path}/out.json"
-    )
-    assert result.exit_code == 0, result.output
-    item = Item.from_file(str(tmp_path / "out.json"))
-    item.validate()
+    item_collection = ItemCollection.from_file(str(tmp_path / "out.json"))
+    for item in item_collection:
+        item.validate()
 
 
 def test_create_collection(tmp_path: Path) -> None:
